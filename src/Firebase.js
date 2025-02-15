94012e1d02996ae4f064c2e0db5c1ad6b1ca234f
manicdon7 <manicdon7@gmail.com>
2025-02-15 22:57:25 +0530

    refactor: update Firebase configuration to use environment variables and switch API URLs to production

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,14 +8,16 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDmZFaIPeqJyQgt7MuvXF5qot2_6I_j_dA",
-  authDomain: "next-step-d6df6.firebaseapp.com",
-  projectId: "next-step-d6df6",
-  storageBucket: "next-step-d6df6.firebasestorage.app",
-  messagingSenderId: "490304033890",
-  appId: "1:490304033890:web:e37eaa7fae29d24224ad3b",
-  measurementId: "G-YRQJBBH45P"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+console.log("Firebase Config:", firebaseConfig);
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const LOCAL_URL = 'http://localhost:5000';
+export const PROD_URL = 'https://nextstep-backend-api.vercel.app';
